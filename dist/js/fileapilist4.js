5570741d982b80600ce1f226e25210e653573d65
sunnydream20 <timonpoo220@gmail.com>
2024-12-16 19:08:38 +0900

    feat: build svg-animation

---
+++ dist/js/fileapilist4.js
@@ -0,0 +1,63 @@
+/** @format */
+
+document.write(
+  '\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltcreatefile.htm"><h5>FltCreateFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltcreatefileex.htm"><h5>FltCreateFileEx</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltcreatefileex2.htm"><h5>FltCreateFileEx2</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltclose.htm"><h5>FltClose</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltreadfile.htm"><h5>FltReadFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltreadfileex.htm"><h5>FltReadFileEx</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltwritefile.htm"><h5>FltWriteFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltwritefileex.htm"><h5>FltWriteFileEx</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltqueryinformationfile.htm"><h5>FltQueryInformationFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltqueryvolumeinformationfile.htm"><h5>FltQueryVolumeInformationFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltqueryvolumeinformation.htm"><h5>FltQueryVolumeInformation</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltqueryinformationbyname.htm"><h5>FltQueryInformationByName</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltquerydirectoryfile.htm"><h5>FltQueryDirectoryFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltqueryeafile.htm"><h5>FltQueryEaFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltquerysecurityobject.htm"><h5>FltQuerySecurityObject</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetinformationfile.htm"><h5>FltSetInformationFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetvolumeinformation.htm"><h5>FltSetVolumeInformation</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltseteafile.htm"><h5>FltSetEaFile</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetsecurityobject.htm"><h5>FltSetSecurityObject</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltgetfilenameinformation.htm"><h5>FltGetFileNameInformation</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltgetdestinationfilenameinformation.htm"><h5>FltGetDestinationFileNameInformation</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltgettunneledname.htm"><h5>FltGetTunneledName</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltparsefilenameinformation.htm"><h5>FltParseFileNameInformation</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltreferencefilenameinformation.htm"><h5>FltReferenceFileNameInformation</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltreleasefilenameinformation.htm"><h5>FltReleaseFileNameInformation</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltcreatesectionfordatascan.htm"><h5>FltCreateSectionForDataScan</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltclosesectionfordatascan.htm"><h5>FltCloseSectionForDataScan</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltstartfiltering.htm"><h5>FltStartFiltering</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltunregisterfilter.htm"><h5>FltUnregisterFilter</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltcreatecommunicationport.htm"><h5>FltCreateCommunicationPort</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltbuilddefaultsecuritydescriptor.htm"><h5>FltBuildDefaultSecurityDescriptor</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltfreesecuritydescriptor.htm"><h5>FltFreeSecurityDescriptor</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-filterconnectcommunicationport.htm"><h5>FilterConnectCommunicationPort</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltcloseclientport.htm"><h5>FltCloseClientPort</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsendmessage.htm"><h5>FltSendMessage</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-filtergetmessage.htm"><h5>FilterGetMessage</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-filterreplymessage.htm"><h5>FilterReplyMessage</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltclosecommunicationport.htm"><h5>FltCloseCommunicationPort</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltattachvolume.htm"><h5>FltAttachVolume</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltdetachvolume.htm"><h5>FltDetachVolume</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltallocatecontext.htm"><h5>FltAllocateContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltreleasecontext.htm"><h5>FltReleaseContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltdeletecontext.htm"><h5>FltDeleteContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetinstancecontext.htm"><h5>FltSetInstanceContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltgetinstancecontext.htm"><h5>FltGetInstanceContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltdeleteinstancecontext.htm"><h5>FltDeleteInstanceContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetvolumecontext.htm"><h5>FltSetVolumeContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltgetvolumecontext.htm"><h5>FltGetVolumeContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltdeletevolumecontext.htm"><h5>FltDeleteVolumeContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetstreamhandlecontext.htm"><h5>FltSetStreamHandleContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltgetstreamhandlecontext.htm"><h5>FltGetStreamHandleContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltdeletestreamhandlecontext.htm"><h5>FltDeleteStreamHandleContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetstreamcontext.htm"><h5>FltSetStreamContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltgetstreamcontext.htm"><h5>FltGetStreamContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltdeletestreamcontext.htm"><h5>FltDeleteStreamContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltsetfilecontext.htm"><h5>FltSetFileContext</h5></a>\
+	<a href="https://www.easefilter.com/kb/fltkernel-fltdeletefilecontext.htm"><h5>FltDeleteFileContext</h5></a>\
+'
+);
